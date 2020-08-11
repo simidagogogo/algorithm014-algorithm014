609ecd99ab351b06a2cfe30c1625c2e906043a9f
Chyqcyq <ch_y_q@outlook.com>
2020-08-11 23:17:04 +0800

    【why week 1 && day 2 "note&&homework" 【when】2020_08_11_23_16

---
+++ Week_01/moveZero_1_20200811.cpp
@@ -0,0 +1,31 @@
+//思想：
+/// 借用额外一个数组（虽说不符合要求）；
+/// 遍历原有数组并将非零值按顺序放到额外数组，并计算出零值个数；
+/// 根据零值个数将额外数组末尾值添加0，并将额外数组拷贝回当前数组
+//时间复杂度：
+//空间复杂度:
+
+class Solution {
+public:
+    void moveZeroes(vector<int>& nums) {		
+		vector<int> tmpNums;
+		int zeroNumsize = 0;
+		for (int i = 0; i < nums.size(); i++) {
+			if (nums[i] != 0) {
+				tmpNums.push_back(nums[i]);
+			} 
+			else {
+				zeroNumsize++;
+			}
+		}
+		while(0 != zeroNumsize--) {
+			tmpNums.push_back(0);
+		}
+
+		for (int i = 0; i < nums.size(); i++) {
+			nums[i] = tmpNums[i];
+			//cout << nums[i] << " ";
+		}
+		//cout << endl;
+    }
+};  
